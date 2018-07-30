ad1ed1f6fa3b5d69016cec74a4073404f7244600
aosunbiyi <adebayo.osunbiyi.a@gmail.com>
2018-07-30 09:16:50 +0100

    setting up the game frame

--- init.py
+++ init.py
@@ -2,11 +2,36 @@ import pygame
 
 pygame.init()
 
-gameDisplay= pygame.display.set_mode((800,600))
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Slither")
 
 pygame.display.update()
 
+gameExit = False
+lead_x = 300
+lead_y = 300
+lead_x_change = 0
+clock = pygame.time.Clock()
+
+
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                lead_x_change = -2
+            if event.key == pygame.K_RIGHT:
+                lead_x_change = 2
+
+    lead_x += lead_x_change
+    gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black, [lead_x, lead_y, 10, 10])
+    pygame.display.update()
+    clock.tick(100)
+
 pygame.quit()
 quit()
-
-
